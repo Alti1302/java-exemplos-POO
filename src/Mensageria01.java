ccd8f5fdb3e3c694133170c469785e8660d6077f
Alti1302 <altieres.silva@hotmail.com>
2024-10-31 23:55:00 -0300

    Exemplos de utilização de herança e polimorfismo

--- src/Mensageria01.java
+++ src/Mensageria01.java
@@ -1,3 +1,10 @@
 public class Mensageria01 extends ServicoMensageria{
+    public void enviarmensagem(){
+        System.out.println("Enviando mensagem pelo serviço 01");
+    }
+    public void recebeMensagem(){
+        System.out.println("Recebendo mensagem pelo serviço 01");
+
+    }
 
 }
--- src/ServicoMensageria.java
+++ src/ServicoMensageria.java
@@ -1,17 +1,5 @@
-public class ServicoMensageria {
-    public void enviarmensagem (){
-        validaConexaoInternet();
-        System.out.println("Enviando mensagem");
-        salvaMensagemHistorico();
-    }
+public abstract class ServicoMensageria {
+   public abstract void  enviarmensagem ();
+   public abstract void  recebeMensagem();
 
-    public void  recebeMensagem (){
-        System.out.println("Recebendo a mensagem");
-    }
-    private void validaConexaoInternet(){
-        System.out.println("Validando conexão");
-    }
-    private void salvaMensagemHistorico(){
-        System.out.println("Salvando mensagem no histórico");
-    }
 }
